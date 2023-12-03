01562c3f0cadaa616c929814f8c4e1cbdad36ba3
arpit2222 <arpit2018cs164@gmail.com>
2023-12-03 22:09:27 +0530

    adding data front page

--- app/my-data/page.tsx
+++ app/my-data/page.tsx
@@ -3,17 +3,43 @@
 import GcpData from '@/components/GcpData';
 import IpfsData from '@/components/IpfsData';
 import Sidebar from '@/components/Sidebar';
-import React, { useState } from 'react';
+import { set } from 'date-fns';
+import React, { useEffect, useState } from 'react';
 
 export default function MyData() {
   const [toggleMenu, setToggleMenu] = useState('gcp');
-  let contentComponent;
+  const [gcpData, setGcpData] = useState([]);
+  const [ipfsData, setIpfsData] = useState<any>([]);
 
-  if (toggleMenu === 'gcp') {
-    contentComponent = <GcpData />;
-  } else if (toggleMenu === 'ipfs') {
-    contentComponent = <IpfsData />;
+  async function fetchDailyRewards() {
+    const walletAddress =localStorage.getItem('userPublicAddress');
+    const userAccessToken =window.localStorage.getItem('userAccessToken');
+    await fetch(
+      `http://localhost:8080/user/api/user-data/${walletAddress}`,
+      {
+        method: 'GET',
+        cache: 'no-cache',
+        headers: {
+          Authorization: `${userAccessToken}`,
+        },
+      }
+    )
+    .then((response) => response.json())
+    .then((data) =>{
+      setGcpData(data.gcp);
+      setIpfsData(data.ipfs[0]);
+      console.log(data);
+    }
+    )
+    .catch((error) => {
+        console.log(error);
+      });
   }
+
+  useEffect(() => {
+    fetchDailyRewards();
+  }, []);
+
   return (
     <div className='flex'>
       <Sidebar />
@@ -25,7 +51,7 @@ export default function MyData() {
                 ? 'bg-white text-black  border-blue-900'
                 : 'bg-blue-900 text-white'
             }`}
-            onClick={() => setToggleMenu('browser-data')}>
+            onClick={() => setToggleMenu('gcp')}>
             Google Cloud
           </div>
           <div
@@ -34,12 +60,27 @@ export default function MyData() {
                 ? 'bg-white text-black  border-blue-900'
                 : 'bg-blue-900 text-white'
             }`}
-            onClick={() => setToggleMenu('email-data')}>
+            onClick={() => setToggleMenu('ipfs')}>
             IPFS
           </div>
         </div>
         <div className='mx-5 w-11/12 md:mx-auto bg-[#e3daf6] rounded flex md:flex-row flex-col p-5 md:h-[75vh] h-[40vh] md:mt-0 mt-16'>
-          {contentComponent}
+          {
+            toggleMenu=="gcp"?
+            <div>
+              {
+                gcpData.map((data,index)=>(
+                <div key={index}>here is the data</div>
+                ))
+              }
+            </div>:<div>
+            {
+                ipfsData.map((data:any,index:number)=>(
+                <div key={index}>{data?.urlLink}</div>
+                ))
+              }
+            </div>
+          }
         </div>
       </div>
     </div>
