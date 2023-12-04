025a3d485a42796d26eb620e4a17155beed7792b
moayaan1911 <ayaangames@gmail.com>
2023-12-04 17:33:05 +0530

    middleware change

--- middleware/Middleware.tsx
+++ middleware/Middleware.tsx
@@ -14,11 +14,62 @@ declare global {
 
 export default function Middleware() {
   const { setUserData } = useContext(AppContext);
+
   const router = useRouter();
   useEffect(() => {
     const storedData = window.localStorage.getItem('dframeUserData');
     console.log('stored data', JSON.parse(storedData as any));
     setUserData(JSON.parse(storedData as any));
+  }, []);
+  // function to connect to polygon mainnet here.
+  const connectToPolygonMainnet = async () => {
+    if ((window as any).ethereum) {
+      const chainId = await (window as any).ethereum.request({
+        method: 'eth_chainId',
+      });
+
+      // Check if connected to a different network (not Polygon mainnet)
+      if (chainId !== '0x89') {
+        // ChainId of Polygon mainnet is '0x89'
+        try {
+          await (window as any).ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: '0x89' }],
+          });
+        } catch (error) {
+          // Handle error
+          console.log('Error while switching to Polygon mainnet:', error);
+        }
+      }
+    } else {
+      // Handle case where window.ethereum is not available (e.g., Metamask is not installed)
+      console.log('Metamask not available');
+    }
+  };
+
+  const checkMetamaskConnection = () => {
+    if (!(window as any).ethereum?.selectedAddress) {
+      // Metamask wallet disconnected, redirect to root route
+      router.push('/');
+    }
+  };
+
+  const handleWalletDisconnect = () => {
+    if (!(window as any).ethereum?.selectedAddress) {
+      // Metamask wallet disconnected
+      router.push('/');
+    }
+  };
+
+  useEffect(() => {
+    // Listen for changes in the selected address property
+    if ((window as any).ethereum) {
+      (window as any).ethereum.on('accountsChanged', handleWalletDisconnect);
+    }
+  }, [(window as any).ethereum]);
+  useEffect(() => {
+    connectToPolygonMainnet();
+    checkMetamaskConnection();
   }, []);
   useEffect(() => {
     if (window.ethereum) {
